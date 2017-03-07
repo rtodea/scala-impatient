d6f3a251f6218485e74df38608e0af0dfad9be85
Robert Todea <robert.todea@e-spres-oh.com>
2017-03-07 10:38:45 +0200

    chapter 1 ex.7

---
+++ src/test/scala/chapter1/Exercise7.scala
@@ -0,0 +1,14 @@
+package chapter1
+
+import org.scalatest.FlatSpec
+import scala.math.BigInt.probablePrime
+import scala.util.Random
+
+class Exercise7 extends FlatSpec {
+  behavior of "importing a specific method"
+
+  it should "import probablePrime and Random" in {
+    val res = probablePrime(100, Random)
+    assert(res.isProbablePrime(0))
+  }
+}
